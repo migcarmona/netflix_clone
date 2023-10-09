9ffac80920d29c3e238f9711ba85b03263b66ad2
migcarmona <migcarmona@gmail.com>
2023-10-09 07:36:55 +0100

    09/10/2023

---
+++ src/components/TopNav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {AiOutlineLogout} from 'react-icons/ai'
+
+const TopNav = () => {
+  return (
+    <div>TopNav</div>
+  )
+}
+
+export default TopNav
--- src/pages/Netflix.js
+++ src/pages/Netflix.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import ageUltron from '../images/age-of-ultron.png'
 
 const Netflix = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  window.onscroll = ()=>{
+    setIsScrolled(window.pageYOffset === 0 ? false : true)
+    return()=>(window.onscroll = null)
+  }
+
+  console.log(isScrolled)
+
   return (
     <div className='hero'>
       <Header/>
