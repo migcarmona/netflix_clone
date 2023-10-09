4f9b3fc781f79de6709675e4048284fd695d8fe3
migcarmona <migcarmona@gmail.com>
2023-10-09 08:24:32 +0100

    09/10/2023 topnav

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import {AiOutlineLogout} from 'react-icons/ai'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const TopNav = () => {
+
+    const navLink = [
+        {name: "Home", link: '/'}
+        {name: "Tv Show", link: '/tv'}
+        {name: "My List", link: '/mylist'}
+        {name: "Movies", link: '/movies'}
+    ]
   return (
     <div>TopNav</div>
   )
