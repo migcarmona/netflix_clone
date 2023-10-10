234cdc757babe0219a3a196e3dc12008590401a5
migcarmona <migcarmona@gmail.com>
2023-10-10 23:38:10 +0100

    Update Player.js

--- src/pages/Player.js
+++ src/pages/Player.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import styled from 'styled-components'
 import {BsArrowLeft} from 'react-icons/bs'
 
+
 const Player = () => {
+
+  const navigate = useNavigate()
   return (
     <PlayContainer>
-      <div>
+      <div className='player'>
+        <div className='backArrow'>
+          <BsArrowLeft onClick={()=>navigate(-1)}/> 
+        </div>
         
       </div>
 
@@ -14,7 +22,14 @@ const Player = () => {
 }
 
 const PlayContainer = styled.div`
+.backArrow{
+  color: white;
+  cursor: pointer;
+  font-size: 2rem;
+  padding: 1rem;
+  margin: 1rem 2rem;
 
+}
 `
 
 export default Player
